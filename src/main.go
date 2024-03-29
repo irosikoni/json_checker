b30fa8039b7f94bbb15959b26f93fe77991fb3ef
irosikoni <irosikoni@gmail.com>
2024-03-30 00:36:55 +0100

    project init, basic verifing resource content done

---
+++ src/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+
+type Statement struct {
+	Sid string
+	Effect string
+	Action []string
+	Resource string
+}
+
+type PolicyDocument struct {
+	Version string
+	Statement []Statement
+}
+
+type IAMRolePolicy struct {
+	PolicyName string
+	PolicyDocument PolicyDocument
+}
+
+func main(){
+	filepath := os.Args[1]
+	policy := loadAndParseJSON(filepath)
+	fmt.Println(policy.isResourceAsterisk())
+}
+
+func loadAndParseJSON(filepath string) IAMRolePolicy{
+	content, loadingErr := os.ReadFile(filepath)
+	if loadingErr != nil {
+		panic(loadingErr)
+	}
+	parsedPolicy := IAMRolePolicy{}
+	parsingError := json.Unmarshal([]byte(content), &parsedPolicy)
+	if parsingError != nil {
+		panic(parsingError)
+	}
+	return parsedPolicy
+}
+
+func (policy IAMRolePolicy) isResourceAsterisk() bool{
+	return policy.PolicyDocument.Statement[0].Resource != "*"
+}
+
