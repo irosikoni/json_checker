4c80a064fa372c3be2a1720e26154f0480c0f890
irosikoni <irosikoni@gmail.com>
2024-03-31 00:06:43 +0100

    all unit tests added

--- src/iamrp/iamrp_test.go
+++ src/iamrp/iamrp_test.go
@@ -31,3 +31,63 @@ func TestIsResourceAsterisk(t *testing.T) {
 		}
 	})
 }
+
+func TestIsResourceAsteriskSub(t *testing.T) {
+	policyWithMissingResource := IAMRolePolicy{
+		PolicyName: "",
+		PolicyDocument: PolicyDocument{
+			Version: "",
+			Statement: []Statement{
+			},
+		},
+	}
+	policyWithTooManyStatements := IAMRolePolicy{
+		PolicyName: "",
+		PolicyDocument: PolicyDocument{
+			Version: "",
+			Statement: []Statement{
+				{
+					Sid: "",
+					Effect: "",
+					Action: []string{""},
+					Resource: "*",
+				},
+				{
+					Sid: "",
+					Effect: "",
+					Action: []string{""},
+					Resource: "*",
+				},
+			},
+		},
+	}
+
+	t.Run("Error checking - missing resource", func(t *testing.T){
+		defer func() {
+			if recovered := recover(); recovered != nil {
+				t.Log("Error checking for missing resource field")
+			}
+		}()
+		policyWithMissingResource.isResourceAsterisk()
+	})
+	t.Run("Error checking - too many statements", func(t *testing.T){
+		defer func() {
+			if recovered := recover(); recovered != nil {
+				t.Log("Error checking for multi-statement policy")
+			}
+		}()
+		policyWithTooManyStatements.isResourceAsterisk()
+	})
+}
+
+func TestLoadAndParseJSON(t *testing.T) {
+	nonexistingFilepath := "thereisnosuchfile"
+	t.Run("Error checking - no file", func(t *testing.T) {
+		defer func() {
+			if recovered := recover(); recovered != nil {
+				t.Log("Error checking for non-existing filepath")
+			}
+		}()
+		loadAndParseJSON(nonexistingFilepath)
+	})
+}
