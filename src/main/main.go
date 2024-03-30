42ea8e515577b74762278aa159d7cdcaebc0660f
irosikoni <irosikoni@gmail.com>
2024-03-30 23:39:43 +0100

    covering errors added

--- src/main/main.go
+++ src/main/main.go
@@ -1,11 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/irosikoni/json_checker/src/iamrp"
 )
 func main(){
 	filepath := os.Args[1]
-	iamrp.IAMRolePolicyValidator(filepath)
+	fmt.Println(iamrp.IAMRolePolicyValidator(filepath))
 }
